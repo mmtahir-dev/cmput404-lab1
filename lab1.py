175b66a003897e76d6fbe1721349693d140a9efd
Mutaal Tahir <muhammadtahir@Mutaals-MacBook-Air.local>
2021-09-10 14:31:30 -0600

    modified

--- lab1.py
+++ lab1.py
@@ -1,4 +1,8 @@
 import sys
 import requests
+from urllib.request import urlopen
 print(requests.__version__)
 print(requests.get("https://www.google.com/"))
+link="https://raw.githubusercontent.com/mmtahir-dev/cmput404-lab1/master/lab1.py"
+r=requests.get(link)
+print(r.text)
